c4d268c399aa42fea916f2ba3fab22500b72a35f
Dean Coulter <d.coulter@cyc.ltd>
2020-08-25 11:32:04 +0100

    fix scroll to

--- src/index.tsx
+++ src/index.tsx
@@ -213,17 +213,28 @@ type SpringAnimationProps = {
   animationConfig?: Partial<Animated.SpringConfig>;
 };
 
+interface State {
+  manuallySetValue: boolean;
+}
+
 type Props<T> = CommonProps &
   (FlatListOption<T> | ScrollViewOption | SectionListOption<T>) &
   (TimingAnimationProps | SpringAnimationProps);
 
-export class ScrollBottomSheet<T extends any> extends Component<Props<T>> {
+export class ScrollBottomSheet<T extends any> extends Component<
+  Props<T>,
+  State
+> {
   static defaultProps = {
     topInset: 0,
     friction: 0.95,
     animationType: 'timing',
     innerRef: React.createRef<AnimatedScrollableComponent>(),
     enableOverScroll: false,
+  };
+
+  public state: State = {
+    manuallySetValue: false,
   };
 
   /**
@@ -672,6 +683,7 @@ export class ScrollBottomSheet<T extends any> extends Component<Props<T>> {
     this.isManuallySetValue.setValue(1);
     this.manualYOffset.setValue(snapPoints[index]);
     this.nextSnapIndex.setValue(index);
+    this.setState({ manuallySetValue: true });
   };
 
   render() {
@@ -828,9 +840,9 @@ export class ScrollBottomSheet<T extends any> extends Component<Props<T>> {
             ])
           )}
         />
-        <Animated.Code
-          exec={onChange(this.isManuallySetValue, [
-            cond(
+        {this.state.manuallySetValue && (
+          <Animated.Code
+            exec={cond(
               this.isManuallySetValue,
               [
                 set(this.destSnapPoint, this.manualYOffset),
@@ -842,12 +854,14 @@ export class ScrollBottomSheet<T extends any> extends Component<Props<T>> {
                   this.masterDrawer?.current?.setNativeProps({
                     maxDeltaY: value - this.getNormalisedSnapPoints()[0],
                   });
+
+                  this.setState({ manuallySetValue: false });
                 }),
               ],
               [set(this.nextSnapIndex, 0)]
-            ),
-          ])}
-        />
+            )}
+          />
+        )}
       </Animated.View>
     );
 
